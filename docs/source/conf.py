9c2cebe83344630f40bb51b664168e46ac438a5f
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-01-21 14:09:52 +0100

    doc: basic setup sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,44 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+from pathlib import Path
+import tomllib
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
+)
 
-project = 'ddm'
-copyright = '2026, x'
-author = 'x'
+pyproject = Path(__file__).parent.parent.parent / "pyproject.toml"
+with open(pyproject, "rb") as f:
+    data = tomllib.load(f)
+
+# extract metadata
+project_meta = data.get("project", {})
+
+project = project_meta.get("name", "Unknown Project")
+author = ", ".join(
+    a.get("name") for a in project_meta.get("authors", []) if a.get("name")
+)
+version = project_meta.get("version", "0.0.0")
+copyright = "2026, " + author
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",  # Markdown parser
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "repository_url": "https://github.com/brunykrijgsman/thesis_ddm",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "path_to_docs": "docs",
+    "navigation_depth": 10,
+}
